fedce09a4cfb17ecf6fc011ede167367f11008f3
Alf Eaton <eaton.alf@gmail.com>
2019-08-17 00:40:43 +0100

    Improve login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
 import { SpotifyClientContext } from '@aeaton/react-spotify'
 import { Button } from '@material-ui/core'
-import React, { useContext } from 'react'
+import React, { useCallback, useContext, useState } from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
@@ -12,11 +12,20 @@ const Container = styled.div`
 `
 
 export const Login = () => {
+  const [error, setError] = useState()
+
   const client = useContext(SpotifyClientContext)
+
+  const handleLogin = useCallback(() => {
+    client.login().catch(error => {
+      setError(error)
+    })
+  }, [client])
 
   return (
     <Container>
-      <Button onClick={client.login}>Sign in with Spotify</Button>
+      {error && <div>{error.message}</div>}
+      <Button onClick={handleLogin}>Sign in with Spotify</Button>
     </Container>
   )
 }
